c1ebba4366ee15a44bd7742d969a4f0465a67929
Max Haland <max@haland.org>
2016-04-11 15:57:07 -0500

    Add case logic to state calculator

--- src/org/usfirst/frc2175/subsystems/powertrain/SwerveModuleStateCalculator.java
+++ src/org/usfirst/frc2175/subsystems/powertrain/SwerveModuleStateCalculator.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc2175.subsystems.powertrain;
 
 import org.usfirst.frc2175.operatorinteraction.DriveMode;
+import org.usfirst.frc2175.velocity.Velocity;
 
 public class SwerveModuleStateCalculator {
 
@@ -17,19 +18,61 @@ public class SwerveModuleStateCalculator {
 
     protected SwerveModuleStateCalculator(ModuleLocation moduleLocation) {
         this.moduleLocation = moduleLocation;
-        this.driveMode = DriveMode.UNICORN;
+        this.driveMode = DriveMode.CRAB;
     }
 
-    public double calculateModuleAngle(double xTranslation, double yTranslation,
-            double rotation) {
+    public void setDriveMode(DriveMode driveMode) {
+        this.driveMode = driveMode;
+    }
+
+    private double calculateModuleAngle(Velocity translationVelocity,
+            double angularVelocity) {
+        double angle = 0;
+        switch (driveMode) {
+        case CRAB:
+            angle = translationVelocity.getDirection();
+            break;
+        case CAR:
+            // car stuff here
+            break;
+        case UNICORN:
+            // unicorn stuff here
+            break;
+        case LOCK:
+            // lock stuff here
+            break;
+        }
+
+        // TODO fill in
+        return angle;
+    }
+
+    private double calculateIdealModuleSpeed(Velocity translationVelocity,
+            double angularVelocity) {
+        double angle = 0;
+        switch (driveMode) {
+        case CRAB:
+            angle = translationVelocity.getMagnitude();
+            break;
+        case CAR:
+            // car stuff here
+            break;
+        case UNICORN:
+            // unicorn stuff here
+            break;
+        case LOCK:
+            angle = 0;
+            break;
+        }
         // TODO fill in
         return 0;
     }
 
-    public double calculateModuleSpeed(double xTranslation, double yTranslation,
-            double rotation) {
-        // TODO fill in
-        return 0;
+    public Velocity getModuleVelocity(Velocity translationVelocity,
+            double angularVelocity) {
+        return new Velocity(
+                calculateIdealModuleSpeed(translationVelocity, angularVelocity),
+                calculateModuleAngle(translationVelocity, angularVelocity));
     }
 
     public ModuleLocation getModuleLocation() {
