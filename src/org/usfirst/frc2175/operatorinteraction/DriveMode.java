366b14952461b9828fa6fc3a38edabc117cd783e
Max Haland <max@haland.org>
2016-04-11 12:48:02 -0500

    Add drive mode enum

---
+++ src/org/usfirst/frc2175/operatorinteraction/DriveMode.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc2175.operatorinteraction;
+
+public enum DriveMode {
+    CRAB, CAR, UNICORN, LOCK;
+}
--- src/org/usfirst/frc2175/subsystems/powertrain/SwerveModuleStateCalculator.java
+++ src/org/usfirst/frc2175/subsystems/powertrain/SwerveModuleStateCalculator.java
@@ -1,14 +1,23 @@
 package org.usfirst.frc2175.subsystems.powertrain;
 
+import org.usfirst.frc2175.operatorinteraction.DriveMode;
+
 public class SwerveModuleStateCalculator {
 
-    private double xTranslation;
-    private double yTranslation;
-    private double rotation;
     private ModuleLocation moduleLocation;
+    private DriveMode driveMode;
 
-    public SwerveModuleStateCalculator(ModuleLocation moduleLocation) {
+    // This should only be used by the powertrain class, with DriveMode
+    // consistent across all modules
+    protected SwerveModuleStateCalculator(ModuleLocation moduleLocation,
+            DriveMode driveMode) {
         this.moduleLocation = moduleLocation;
+        this.driveMode = driveMode;
+    }
+
+    protected SwerveModuleStateCalculator(ModuleLocation moduleLocation) {
+        this.moduleLocation = moduleLocation;
+        this.driveMode = DriveMode.UNICORN;
     }
 
     public double calculateModuleAngle(double xTranslation, double yTranslation,
