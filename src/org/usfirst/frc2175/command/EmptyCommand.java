dcfd2bd1704d1df9b7d75300c16d58221d353596
Max Haland <max@haland.org>
2016-04-11 17:13:47 -0500

    Add base and empty commands

---
+++ src/org/usfirst/frc2175/command/EmptyCommand.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc2175.command;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class EmptyCommand extends Command {
+
+    public EmptyCommand() {
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+}
