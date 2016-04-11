3e52331a476d32ac198e1ac6c15195237e9d0f4c
Max Haland <max@haland.org>
2016-04-11 15:56:47 -0500

    Change joystick interpreter to use Velocities

--- src/org/usfirst/frc2175/operatorinteraction/JoystickInterpreter.java
+++ src/org/usfirst/frc2175/operatorinteraction/JoystickInterpreter.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc2175.operatorinteraction;
 
+import org.usfirst.frc2175.velocity.Velocity;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 public class JoystickInterpreter {
@@ -12,15 +14,46 @@ public class JoystickInterpreter {
         this.rotationJoystick = rotationJoystick;
     }
 
-    public double getXTranslation() {
+    public Velocity getTranslationVelocity() {
+        Velocity translationVelocity = new Velocity(
+                getTranslationJoystickMagnitude(), getRotationJoystickAngle());
+        return translationVelocity;
+    }
+
+    private double getXTranslation() {
         return translationJoystick.getX();
     }
 
-    public double getYTranslation() {
+    private double getYTranslation() {
         return translationJoystick.getY();
     }
 
-    public double getRotationJoystickAngle() {
+    private double getTranslationJoystickAngle() {
+        // TODO abstract this, used several times
+        double angle = 0;
+        double x = translationJoystick.getX();
+        double y = translationJoystick.getY();
+        double atan = Math.abs(Math.atan(y / x));
+        // Handle quadrants
+        if (y >= 0 && x >= 0) {
+            angle = atan;
+        } else if (y >= 0 && x < 0) {
+            angle = atan + Math.PI / 2;
+        } else if (y < 0 && x < 0) {
+            angle = atan + Math.PI;
+        } else if (y < 0 && x >= 0) {
+            angle = atan + (3 * Math.PI / 2);
+        }
+        return angle;
+    }
+
+    private double getTranslationJoystickMagnitude() {
+        double x = translationJoystick.getX();
+        double y = translationJoystick.getY();
+        return Math.sqrt((x * x) + (y * y));
+    }
+
+    private double getRotationJoystickAngle() {
         double angle = 0;
         double x = rotationJoystick.getX();
         double y = rotationJoystick.getY();
